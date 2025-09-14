f4e918a72e5d0275af1de80ae53122fda2140d3f
yousseffdil <fdilyoussef@gmail.com>
2025-09-14 22:13:54 +0200

    Enhance Astro configuration with Tailwind CSS and Astro Icon integration; remove Welcome component and update index page to use new components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+import icon from "astro-icon";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
+});
